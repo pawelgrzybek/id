767d6b5fb2eed7528e5fc52c159b374f5f2153d2
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2022-09-22 15:14:29 +0100

    update

---
+++ bin/card.mjs
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+"use strict";
+
+console.log("\n");
+console.log("Pawel Grzybek · Software Engineer at Polygon");
+console.log("https://pawelgrzybek.com/");
+console.log("https://twitter.com/pawelgrzybek");
+console.log("https://github.com/pawelgrzybek");
+console.log("\n");
