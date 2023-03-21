b8ef8bd99176c06a66880c19363fb60667ed43f0
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2023-03-21 10:50:06 +0000

    Update twitter and mastodon link

--- bin/card.mjs
+++ bin/card.mjs
@@ -5,6 +5,7 @@
 console.log("\n");
 console.log("Pawel Grzybek · Software Engineer at Polygon");
 console.log("https://pawelgrzybek.com/");
-console.log("https://twitter.com/pawelgrzybek");
+console.log("h̶t̶t̶p̶s̶:̶/̶/̶t̶w̶i̶t̶t̶e̶r̶.̶c̶o̶m̶/̶p̶a̶w̶e̶l̶g̶r̶z̶y̶b̶e̶k̶");
+console.log("https://mastodon.social/@pawelgrzybek");
 console.log("https://github.com/pawelgrzybek");
 console.log("\n");
